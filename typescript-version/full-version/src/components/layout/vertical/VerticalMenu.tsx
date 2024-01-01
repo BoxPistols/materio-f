c79c922dde6462039bc10b1e1ab7519ff3a81f7b
Omkar Oza <ozaomkar2011@gmail.com>
2024-01-01 13:18:38 +0530

    refactor(menu): replace menu's dynamic data with static data

--- typescript-version/full-version/src/components/layout/vertical/VerticalMenu.tsx
+++ typescript-version/full-version/src/components/layout/vertical/VerticalMenu.tsx
@@ -14,7 +14,10 @@ import type { VerticalMenuContextProps } from '@menu-package/components/vertical
 import type { Dictionary } from '@core/types'
 
 // Component Imports from @menu-package
-import { Menu } from '@menu-package/vertical-menu'
+import { Menu, SubMenu, MenuItem, MenuSection } from '@menu-package/vertical-menu'
+
+// Icon Imports
+import LinkExternalIcon from '@layouts/svg/LinkExternal'
 
 // Hook Imports
 import useVerticalNav from '@menu-package/hooks/useVerticalNav'
@@ -22,7 +25,7 @@ import useVerticalNav from '@menu-package/hooks/useVerticalNav'
 // Util Imports
 import { getLocale } from '@/utils/get-locale'
 
-import { generateVerticalMenu } from '@/utils/menuUtils'
+// import { generateVerticalMenu } from '@/utils/menuUtils'
 
 // Styled Component Imports
 import StyledVerticalNavExpandIcon from '@menu-package/styles/vertical/StyledVerticalNavExpandIcon'
@@ -32,7 +35,7 @@ import menuItemStyles from '@core/styles/vertical/menuItemStyles'
 import menuSectionStyles from '@core/styles/vertical/menuSectionStyles'
 
 // Menu Data Imports
-import menuData from '@/data/navigation/verticalMenuData'
+// import menuData from '@/data/navigation/verticalMenuData'
 
 type RenderExpandIconProps = {
   open?: boolean
@@ -70,7 +73,6 @@ const VerticalMenu = ({ dictionary }: { dictionary: Dictionary }) => {
     >
       {/* Incase you also want to scroll NavHeader to scroll with Vertical Menu, remove NavHeader from above and paste it below this comment */}
       {/* Vertical Menu */}
-
       <Menu
         popoutMenuOffset={{ mainAxis: 10 }}
         menuItemStyles={menuItemStyles(verticalNavOptions, theme)}
@@ -78,8 +80,211 @@ const VerticalMenu = ({ dictionary }: { dictionary: Dictionary }) => {
         renderExpandedMenuItemIcon={{ icon: <i className='ri-circle-line' /> }}
         menuSectionStyles={menuSectionStyles(verticalNavOptions, theme)}
       >
-        {generateVerticalMenu(menuData(dictionary, params), locale)}
+        <SubMenu label={dictionary['navigation'].dashboards} icon={<i className='ri-home-smile-line' />}>
+          <MenuItem href={`/${locale}/dashboards/analytics`}>{dictionary['navigation'].analytics}</MenuItem>
+          <MenuItem href={`/${locale}/dashboards/ecommerce`}>{dictionary['navigation'].eCommerce}</MenuItem>
+          <MenuItem href={`/${locale}/dashboards/crm`}>{dictionary['navigation'].CRM}</MenuItem>
+        </SubMenu>
+        <MenuSection label={dictionary['navigation'].appsPages}>
+          <MenuItem href={`/${locale}/apps/calendar`} icon={<i className='ri-calendar-line' />}>
+            {dictionary['navigation'].calendar}
+          </MenuItem>
+          <SubMenu label={dictionary['navigation'].invoice} icon={<i className='ri-bill-line' />}>
+            <MenuItem href={`/${locale}/apps/invoice/list`}>{dictionary['navigation'].list}</MenuItem>
+            <MenuItem href={`/${locale}/apps/invoice/preview/${params.id || '4987'}`}>
+              {dictionary['navigation'].preview}
+            </MenuItem>
+            <MenuItem href={`/${locale}/apps/invoice/edit/${params.id || '4987'}`}>
+              {dictionary['navigation'].edit}
+            </MenuItem>
+            <MenuItem href={`/${locale}/apps/invoice/add`}>{dictionary['navigation'].add}</MenuItem>
+          </SubMenu>
+          <SubMenu label={dictionary['navigation'].user} icon={<i className='ri-user-line'/>}>
+            <MenuItem href={`/${locale}/apps/user/list`}>{dictionary['navigation'].list}</MenuItem>
+            <MenuItem href={`/${locale}/apps/user/view`}>{dictionary['navigation'].view}</MenuItem>
+          </SubMenu>
+          <SubMenu label={dictionary['navigation'].rolesPermissions} icon={<i className='ri-lock-2-line'/>}>
+            <MenuItem href={`/${locale}/apps/roles`}>{dictionary['navigation'].roles}</MenuItem>
+            <MenuItem href={`/${locale}/apps/permissions`}>{dictionary['navigation'].permissions}</MenuItem>
+          </SubMenu>
+          <SubMenu label={dictionary['navigation'].pages} icon={<i className='ri-layout-left-line'/>}>
+            <MenuItem href={`/${locale}/pages/user-profile`}>{dictionary['navigation'].userProfile}</MenuItem>
+            <MenuItem href={`/${locale}/pages/account-settings`}>{dictionary['navigation'].accountSettings}</MenuItem>
+            <MenuItem href={`/${locale}/pages/faq`}>{dictionary['navigation'].faq}</MenuItem>
+            <MenuItem href={`/${locale}/pages/pricing`}>{dictionary['navigation'].pricing}</MenuItem>
+            <SubMenu label={dictionary['navigation'].miscellaneous}>
+              <MenuItem href={`/${locale}/pages/misc/coming-soon`} target='_blank'>
+                {dictionary['navigation'].comingSoon}
+              </MenuItem>
+              <MenuItem href={`/${locale}/pages/misc/under-maintenance`} target='_blank'>
+                {dictionary['navigation'].underMaintenance}
+              </MenuItem>
+              <MenuItem href={`/${locale}/pages/misc/404-not-found`} target='_blank'>
+                {dictionary['navigation'].pageNotFound404}
+              </MenuItem>
+              <MenuItem href={`/${locale}/pages/misc/401-not-authorized`} target='_blank'>
+                {dictionary['navigation'].notAuthorized401}
+              </MenuItem>
+            </SubMenu>
+          </SubMenu>
+          <SubMenu label={dictionary['navigation'].authPages} icon={<i className='ri-shield-keyhole-line'/>}>
+            <SubMenu label={dictionary['navigation'].login}>
+              <MenuItem href={`/${locale}/pages/auth/login-v1`} target='_blank'>
+                {dictionary['navigation'].loginV1}
+              </MenuItem>
+              <MenuItem href={`/${locale}/pages/auth/login-v2`} target='_blank'>
+                {dictionary['navigation'].loginV2}
+              </MenuItem>
+            </SubMenu>
+            <SubMenu label={dictionary['navigation'].register}>
+              <MenuItem href={`/${locale}/pages/auth/register-v1`} target='_blank'>
+                {dictionary['navigation'].registerV1}
+              </MenuItem>
+              <MenuItem href={`/${locale}/pages/auth/register-v2`} target='_blank'>
+                {dictionary['navigation'].registerV2}
+              </MenuItem>
+              <MenuItem href={`/${locale}/pages/auth/register-multi-steps`} target='_blank'>
+                {dictionary['navigation'].registerMultiSteps}
+              </MenuItem>
+            </SubMenu>
+            <SubMenu label={dictionary['navigation'].verifyEmail}>
+              <MenuItem href={`/${locale}/pages/auth/verify-email-v1`} target='_blank'>
+                {dictionary['navigation'].verifyEmailV1}
+              </MenuItem>
+              <MenuItem href={`/${locale}/pages/auth/verify-email-v2`} target='_blank'>
+                {dictionary['navigation'].verifyEmailV2}
+              </MenuItem>
+            </SubMenu>
+            <SubMenu label={dictionary['navigation'].forgotPassword}>
+              <MenuItem href={`/${locale}/pages/auth/forgot-password-v1`} target='_blank'>
+                {dictionary['navigation'].forgotPasswordV1}
+              </MenuItem>
+              <MenuItem href={`/${locale}/pages/auth/forgot-password-v2`} target='_blank'>
+                {dictionary['navigation'].forgotPasswordV2}
+              </MenuItem>
+            </SubMenu>
+            <SubMenu label={dictionary['navigation'].resetPassword}>
+              <MenuItem href={`/${locale}/pages/auth/reset-password-v1`} target='_blank'>
+                {dictionary['navigation'].resetPasswordV1}
+              </MenuItem>
+              <MenuItem href={`/${locale}/pages/auth/reset-password-v2`} target='_blank'>
+                {dictionary['navigation'].resetPasswordV2}
+              </MenuItem>
+            </SubMenu>
+            <SubMenu label={dictionary['navigation'].twoSteps}>
+              <MenuItem href={`/${locale}/pages/auth/two-steps-v1`} target='_blank'>
+                {dictionary['navigation'].twoStepsV1}
+              </MenuItem>
+              <MenuItem href={`/${locale}/pages/auth/two-steps-v2`} target='_blank'>
+                {dictionary['navigation'].twoStepsV2}
+              </MenuItem>
+            </SubMenu>
+          </SubMenu>
+          <SubMenu label={dictionary['navigation'].wizardExamples} icon={<i className='ri-git-commit-line'/>}>
+            <MenuItem href={`/${locale}/pages/wizard-examples/checkout`}>{dictionary['navigation'].checkout}</MenuItem>
+            <MenuItem href={`/${locale}/pages/wizard-examples/property-listing`}>
+              {dictionary['navigation'].propertyListing}
+            </MenuItem>
+            <MenuItem href={`/${locale}/pages/wizard-examples/create-deal`}>
+              {dictionary['navigation'].createDeal}
+            </MenuItem>
+          </SubMenu>
+          <MenuItem href={`/${locale}/pages/dialog-examples`} icon={<i className='ri-tv-2-line'/>}>{dictionary['navigation'].dialogExamples}</MenuItem>
+          <SubMenu label={dictionary['navigation'].widgetExamples} icon={<i className='ri-bar-chart-box-line'/>}>
+            <MenuItem href={`/${locale}/pages/widget-examples/advanced`}>{dictionary['navigation'].advanced}</MenuItem>
+            <MenuItem href={`/${locale}/pages/widget-examples/statistics`}>
+              {dictionary['navigation'].statistics}
+            </MenuItem>
+            <MenuItem href={`/${locale}/pages/widget-examples/charts`}>{dictionary['navigation'].charts}</MenuItem>
+          </SubMenu>
+          <MenuItem href={`/${locale}/icons-test`} icon={<i className='ri-remixicon-line'/>}>Icons Test</MenuItem>
+        </MenuSection>
+        <MenuSection label={dictionary['navigation'].formsAndTables} >
+          <MenuItem href={`/${locale}/forms/form-layouts`} icon={<i className='ri-layout-4-line'/>}>{dictionary['navigation'].formLayouts}</MenuItem>
+          <MenuItem href={`/${locale}/forms/form-validation`} icon={<i className='ri-checkbox-multiple-line'/>}>{dictionary['navigation'].formValidation}</MenuItem>
+          <MenuItem href={`/${locale}/forms/form-wizard`} icon={<i className='ri-git-commit-line'/>}>{dictionary['navigation'].formWizard}</MenuItem>
+          <MenuItem href={`/${locale}/react-table`} icon={<i className='ri-table-alt-line'/>}>{dictionary['navigation'].reactTable}</MenuItem>
+          <MenuItem
+            href='http://localhost:3001/docs/forms-tables/intro#form-elements'
+            suffix={<LinkExternalIcon fontSize='1.125rem' />}
+            target='_blank'
+            icon={<i className='ri-radio-button-line'/>}
+          >
+            {dictionary['navigation'].formELements}
+          </MenuItem>
+          <MenuItem
+            href='http://localhost:3001/docs/forms-tables/intro#tables'
+            suffix={<LinkExternalIcon fontSize='1.125rem' />}
+            target='_blank'
+            icon={<i className='ri-table-2'/>}
+          >
+            {dictionary['navigation'].muiTables}
+          </MenuItem>
+        </MenuSection>
+        <MenuSection label={dictionary['navigation'].chartsMisc}>
+          <SubMenu label={dictionary['navigation'].charts} icon={<i className='ri-bar-chart-2-line'/>}>
+            <MenuItem href={`/${locale}/charts/recharts`} >{dictionary['navigation'].recharts}</MenuItem>
+            <MenuItem href={`/${locale}/charts/apex-charts`}>{dictionary['navigation'].apex}</MenuItem>
+          </SubMenu>
+          <MenuItem
+            href='http://localhost:3001/docs/user-interface/intro'
+            suffix={<LinkExternalIcon fontSize='1.125rem' />}
+            target='_blank'
+            icon={<i className='ri-pantone-line'/>}
+          >
+            {dictionary['navigation'].userInterface}
+          </MenuItem>
+          <MenuItem
+            href='http://localhost:3001/docs/components/intro'
+            suffix={<LinkExternalIcon fontSize='1.125rem' />}
+            target='_blank'
+            icon={<i className='ri-toggle-line'/>}
+          >
+            {dictionary['navigation'].components}
+          </MenuItem>
+          <MenuItem
+            href='http://localhost:3001/docs/menu-examples/'
+            suffix={<LinkExternalIcon fontSize='1.125rem' />}
+            target='_blank'
+            icon={<i className='ri-menu-search-line'/>}
+          >
+            {dictionary['navigation'].menuExamples}
+          </MenuItem>
+          <MenuItem suffix={<LinkExternalIcon fontSize='1.125rem' />} target='_blank' icon={<i className='ri-lifebuoy-line'/>}>
+            {dictionary['navigation'].raiseSupport}
+          </MenuItem>
+          <MenuItem suffix={<LinkExternalIcon fontSize='1.125rem' />} target='_blank' icon={<i className='ri-book-line'/>}>
+            {dictionary['navigation'].documentation}
+          </MenuItem>
+          <SubMenu label={dictionary['navigation'].others} icon={<i className='ri-more-line'/>}>
+            <MenuItem suffix='2️⃣'>{dictionary['navigation'].itemWithBadge}</MenuItem>
+            <MenuItem
+              href='https://themeselection.com/'
+              target='_blank'
+              suffix={<LinkExternalIcon fontSize='1.125rem' />}
+            >
+              {dictionary['navigation'].externalLink}
+            </MenuItem>
+            <SubMenu label={dictionary['navigation'].menuLevels}>
+              <MenuItem>{dictionary['navigation'].menuLevel2}</MenuItem>
+              <SubMenu label={dictionary['navigation'].menuLevel2}>
+                <MenuItem>{dictionary['navigation'].menuLevel3}</MenuItem>
+                <MenuItem>{dictionary['navigation'].menuLevel3}</MenuItem>
+              </SubMenu>
+            </SubMenu>
+            <MenuItem disabled>{dictionary['navigation'].disabledMenu}</MenuItem>
+          </SubMenu>
+        </MenuSection>
       </Menu>
+      {/* <Menu
+        popoutMenuOffset={{ mainAxis: 10 }}
+        menuItemStyles={menuItemStyles(verticalNavOptions, theme)}
+        renderExpandIcon={({ open }) => <RenderExpandIcon open={open} transitionDuration={transitionDuration} />}
+        renderExpandedMenuItemIcon={{ icon: <i className='ri-circle-line' /> }}
+        menuSectionStyles={menuSectionStyles(verticalNavOptions, theme)}
+      >
+        {generateVerticalMenu(menuData(dictionary, params), locale)}
+      </Menu> */}
     </ScrollWrapper>
   )
 }
